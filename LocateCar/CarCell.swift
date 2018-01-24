a3ea6d64f5af2da685610b3597f2b9a782ee9c74
Mashqur <mashqur.himel@gmail.com>
2018-01-24 18:12:08 +0600

    initial commit

---
+++ LocateCar/CarCell.swift
@@ -0,0 +1,15 @@
+
+
+import Foundation
+import  UIKit
+
+class CarCell: UITableViewCell {
+    
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var vin: UILabel!
+    @IBOutlet weak var fuel: UILabel!
+    @IBOutlet weak var interior: UILabel!
+    @IBOutlet weak var exterior: UILabel!
+    @IBOutlet weak var address: UILabel!
+    @IBOutlet weak var engineType: UILabel!
+}
